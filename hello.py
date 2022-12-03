7550b89949f4e320f5aa977e0a2a5a779c25e3b1
Yethu <yethukichu55@gmail.com>
2022-12-03 22:16:15 +0530

    remove_background functionalty completed

--- hello.py
+++ hello.py
@@ -16,13 +16,33 @@ def open_file():
     if input_path:
         my_img = ImageTk.PhotoImage(Image.open(input_path))
         my_label.config(image=my_img,bg="black")
+def remove_file():
+    global output_path
+    output_path = filedialog.asksaveasfilename(title="Save as",
+     filetypes=(("png files", ".png"), ("all files", "*.*")))
+
+    input =Image.open(input_path)
+
+    output = remove() 
+
+    output.save(output_path,"png")
+
+    global my_img
+    my_img = ImageTk.PhotoImage(Image.open(output_path))
+
+
+    my_label.config(Image=my_img)
+
+
+
+
 
 my_label = Label(root, text="Remove Background", font=("Arial", 20))
 my_label.pack(pady=20)
 
-open_Button = Button(root, text="Select Image", command="select_image")
+open_Button = Button(root, text="Select Image", command=open_file)
 open_Button.pack(pady=20)
 
-rem_Button = Button(root, text="remove Image", command="remove_image")
+rem_Button = Button(root, text="remove Image", command=remove_file)
 rem_Button.pack(pady=20)
 root.mainloop()
